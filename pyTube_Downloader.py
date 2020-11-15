74f29c9955784eae7fdb89fc2bdc7087db6f9d17
Manick Pillai <manickpillai.20@gmail.com>
2020-11-15 10:57:02 -0500

    updated selecting download for video which has embedded audio

--- pyTube_Downloader.py
+++ pyTube_Downloader.py
@@ -4,23 +4,23 @@ import pytube
     File are saved in downloadPath
 """
 
-url = input("Enter the youtube URL ")
+url = input("\nEnter the youtube URL ")
 downloadPath = r"/Users/user/Downloads"
+audio = "mp4a.40.2"
 iTag = ""
 
 try:
     youtube = pytube.YouTube(url)
-    print(f'The youtube title is "{youtube.title}"\n')
+    print(f'\nThe youtube title is "{youtube.title}"\n')
     streams = youtube.streams
-
     audioVideo = input("Enter 'V' for video 'A' for audio ")
     if len(audioVideo) == 0 or audioVideo.upper() == 'V':
         i = "video/mp4"
     else:
         i = "audio/mp4"
-    print(f'The available streams for {i}')
+    print(f'\nThe available streams for {i}\n')
     for entry in streams:
-        if i in str(entry):
+        if i in str(entry) and audio in str(entry):
             try:
                 print(entry)
             except:
@@ -28,12 +28,12 @@ try:
                 iTag = None
     if iTag != None:
         try:
-            iTag = int(input("Enter the iTag number "))
-            print(f'Downloading video {youtube.title} with iTag {iTag}')
+            iTag = int(input("\nEnter the iTag number "))
+            print(f'\nDownloading video {youtube.title} with iTag {iTag}')
             video = youtube.streams.get_by_itag(iTag)
             video.download(downloadPath)
-            print(f'Downloaded file to path {downloadPath}')
+            print(f'\nDownloaded file to path {downloadPath}')
         except:
-            print('iTag not available')
+            print('\niTag not available')
 except:
     print('URL provided is not recogonized. Exiting program')
